4d8ec8dd42b9e219f75ad7dd789ad03b1749a254
jujopico <sujunqt@gmail.com>
2019-10-26 15:39:35 -0400

    search bar works

--- client/src/pages/GameList.js
+++ client/src/pages/GameList.js
@@ -1,9 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const GameList = () => (
-    <div className="gamelist">
-        <h1>This is the GameList Page</h1>
+class GameList extends React.Component {
+
+state = { games: [] }
+
+handleSearch = event => {
+  event.preventDefault()
+  fetch(`https://api.rawg.io/api/games?search=${event.target.value}`)
+    .then(response => response.json())
+    .then(data => this.setState({ games: data.results || [] }))
+}
+
+render(){
+    console.log(this.state.games)
+  return(
+    <div className="game-list">
+      <h1>Search for a game</h1>
+      <input
+        type="text"
+        onChange={this.handleSearch}
+      />
+
+
+      <div className="results">
+        {
+          this.state.games.map(game => (
+            <Link key={game.id} to={`/games/${game.id}`}>
+              <div className="game">
+                <h3>{game.name}</h3>
+                <img src={game.background_image} alt={game.name} />
+              </div>
+            </Link>
+          ))
+        }
+      </div>
     </div>
-)
+  )
+    }
+}
+
 export default GameList
