c6bff36f63c4247ff19a9d016c09cc241444e5c3
Jennifer Jordan <jbaldwin4@hotmail.com>
2019-10-26 16:00:34 -0400

    fixed indentation and commented out css

--- client/src/pages/GameList.js
+++ client/src/pages/GameList.js
@@ -3,41 +3,41 @@ import { Link } from 'react-router-dom'
 
 class GameList extends React.Component {
 
-state = { games: [] }
+  state = { games: [] }
 
-handleSearch = event => {
-  event.preventDefault()
-  fetch(`https://api.rawg.io/api/games?search=${event.target.value}`)
-    .then(response => response.json())
-    .then(data => this.setState({ games: data.results || [] }))
-}
+  handleSearch = event => {
+    event.preventDefault()
+    fetch(`https://api.rawg.io/api/games?search=${event.target.value}`)
+      .then(response => response.json())
+      .then(data => this.setState({ games: data.results || [] }))
+  }
 
-render(){
-    console.log(this.state.games)
-  return(
-    <div className="game-list">
-      <h1>Search for a game</h1>
-      <input
-        type="text"
-        onChange={this.handleSearch}
-      />
+  render() {
+      console.log(this.state.games)
+    return(
+      <div className="game-list">
+        <h1>Search for a game</h1>
+        <input
+          type="text"
+          onChange={this.handleSearch}
+        />
 
 
-      <div className="results">
-        {
-          this.state.games.map(game => (
-            <Link key={game.id} to={`/games/${game.id}`}>
-              <div className="game">
-                <h3>{game.name}</h3>
-                <img src={game.background_image} alt={game.name} />
-              </div>
-            </Link>
-          ))
-        }
+        <div className="results">
+          {
+            this.state.games.map(game => (
+              <Link key={game.id} to={`/games/${game.id}`}>
+                <div className="game">
+                  <h3>{game.name}</h3>
+                  <img src={game.background_image} alt={game.name} />
+                </div>
+              </Link>
+            ))
+          }
+        </div>
       </div>
-    </div>
-  )
-    }
+    )
+  }
 }
 
 export default GameList
