85174383352c394dec5115524772c48fd3825427
jujopico <sujunqt@gmail.com>
2019-10-26 20:08:20 -0400

    try to fetch single game details

--- client/src/pages/SingleGame.js
+++ client/src/pages/SingleGame.js
@@ -1,8 +1,46 @@
 import React from 'react'
 
-const SingleGame = () => (
-    <div className="singlegame">
-        <h1>This is the Single Game Page</h1>
-    </div>
-)
+class SingleGame extends React.Component {
+  state = { game: [] }
+
+  fetchGame = () => {
+    fetch(`https://api.rawg.io/api/games/${this.props.match.params.id}`)
+      .then(response => response.json())
+      .then(game => this.setState({ game })
+        )
+  }
+
+  render(){
+    return(
+      <div className="single-game">
+        <h1>{this.state.game.name}</h1>
+        <div className="game-data">
+          <img src={this.state.game.background_image} alt={this.state.game.name}/>
+           { <table>
+            <tbody>
+              {
+                ["released", "rating", "platforms"].map(dataPoint => (
+                  <tr key={dataPoint}>
+                    <td>{dataPoint}</td>
+                    <td>{this.state.game[dataPoint]}</td>
+                  </tr>
+                ))
+              }
+              <tr>
+                <td>Rating</td>
+                <td>{this.state.game.Metascore}/100</td>
+              </tr>
+            </tbody>
+          </table>}
+        </div>
+
+      </div>
+    )
+  }
+
+  componentDidMount(){
+    this.fetchGame()
+  }
+}
+
 export default SingleGame
